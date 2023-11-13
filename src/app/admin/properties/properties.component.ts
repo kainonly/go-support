8bfd87ab4d5192f56d3735fb1fae278d3950d15a
kain <zhangtqx@vip.qq.com>
2023-11-13 19:36:33 +0800

    update: Initialization project

---
+++ src/app/admin/properties/properties.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-properties',
+  templateUrl: './properties.component.html'
+})
+export class PropertiesComponent {}
