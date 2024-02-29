ce5e2243f9b2c50b02984e47cffc1777ccfe38f9
kain <kainpro@foxmail.com>
2024-02-29 16:01:56 +0800

    update: Initialization experiment

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,20 +3,26 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+(async () => {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      connectionTimeout: 15 * 1000,
+      jsonShorthand: false
+    })
   );
-}
-
-bootstrap();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidUnknownValues: true
+    })
+  );
+  const port = process.env.PORT || 6000;
+  await app.listen(port);
+})();
