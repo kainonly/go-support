ba59f329cdbd525a6f3c41b213a7f20296ee84be
kain <kainpro@foxmail.com>
2024-03-02 19:19:49 +0800

    update: init

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,19 +12,6 @@ import { UsersService } from './users/users.service';
     ConfigModule.forRoot({
       envFilePath: ['.env'],
       isGlobal: true
-    }),
-    TypeOrmModule.forRootAsync({
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST', '127.0.0.1'),
-        port: config.get('DB_PORT', 3306),
-        username: config.get('DB_USERNAME', 'postgres'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_NAME'),
-        entities: [],
-        synchronize: true
-      }),
-      inject: [ConfigService]
     }),
     UsersModule
   ],
